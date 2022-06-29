a71d010ae5b02fbe4abe0b4600197f089f6b48d9
NicKoehler <grillinicolavocal@gmail.com>
2022-06-29 10:28:18 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["requests"],
+    install_requires=["requests", "pkce"],
 )
